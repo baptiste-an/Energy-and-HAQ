f6ec5e7102693619d7ad516dfd994d317907660d
baptiste-an <baptiste.andrieu@protonmail.com>
2022-04-07 21:01:32 +0200

    s

--- health1.py
+++ health1.py
@@ -12,6 +12,311 @@ cc = coco.CountryConverter()
 pathexio = "C:/Users/andrieba/Documents/"
 
 
+# ........CREATE FUNCTION TO CONVERT ANY REGION NAME FORMAT TO A COMMON FORMAT................
+
+dict_regions = dict()  # create a dict that will be used to rename regions
+cc = coco.CountryConverter(
+    include_obsolete=True
+)  # documentation for coco here: https://github.com/konstantinstadler/country_converter
+for i in [
+    n for n in cc.valid_class if n != "name_short"
+]:  # we convert all the regions in cc to name short and add it to the dict
+    dict_regions.update(cc.get_correspondence_dict(i, "name_short"))
+name_short = cc.ISO3as("name_short")[
+    "name_short"
+].values  # array containing all region names in short_name format
+
+
+def dict_regions_update():
+    """Adds to dict the encountered region names that were not in coco.
+
+    If a region is wider than a country (for example "European Union"), it is added to "Z - Aggregated categories" in order to be deleted later.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    dict_regions["Bolivia (Plurinational State of)"] = "Bolivia"
+    dict_regions["Czechia"] = "Czech Republic"
+    dict_regions["Iran (Islamic Republic of)"] = "Iran"
+    dict_regions["China, Taiwan Province of China"] = "Taiwan"
+    dict_regions["Congo"] = "Congo Republic"
+    dict_regions["Venezuela (Bolivarian Republic of)"] = "Venezuela"
+    dict_regions["Dem. People's Republic of Korea"] = "North Korea"
+    dict_regions["Bahamas, The"] = "Bahamas"
+    dict_regions["Congo, Dem. Rep."] = "DR Congo"
+    dict_regions["Congo, Rep."] = "Congo Republic"
+    dict_regions["Egypt, Arab Rep."] = "Egypt"
+    dict_regions["Faroe Islands"] = "Faeroe Islands"
+    dict_regions["Gambia, The"] = "Gambia"
+    dict_regions["Hong Kong SAR, China"] = "Hong Kong"
+    dict_regions["Iran, Islamic Rep."] = "Iran"
+    dict_regions["Korea, Dem. People's Rep."] = "North Korea"
+    dict_regions["Korea, Rep."] = "South Korea"
+    dict_regions["Lao PDR"] = "Laos"
+    dict_regions["Macao SAR, China"] = "Macau"
+    dict_regions["North Macedonia"] = "Macedonia"
+    dict_regions["Russian Federation"] = "Russia"
+    dict_regions["Sint Maarten (Dutch part)"] = "Sint Maarten"
+    dict_regions["Slovak Republic"] = "Slovakia"
+    dict_regions["St. Martin (French part)"] = "Saint-Martin"
+    dict_regions["Syrian Arab Republic"] = "Syria"
+    dict_regions["Virgin Islands (U.S.)"] = "United States Virgin Islands"
+    dict_regions["West Bank and Gaza"] = "Palestine"
+    dict_regions["Yemen, Rep."] = "Yemen"
+    dict_regions["Venezuela, RB"] = "Venezuela"
+    dict_regions["Brunei"] = "Brunei Darussalam"
+    dict_regions["Cape Verde"] = "Cabo Verde"
+    dict_regions["Dem. People's Rep. Korea"] = "North Korea"
+    dict_regions["Swaziland"] = "Eswatini"
+    dict_regions["Taiwan, China"] = "Taiwan"
+    dict_regions["Virgin Islands"] = "United States Virgin Islands"
+    dict_regions["Yemen, PDR"] = "Yemen"
+    dict_regions["Réunion"] = "Reunion"
+    dict_regions["Saint Helena"] = "St. Helena"
+    dict_regions["China, Hong Kong SAR"] = "Hong Kong"
+    dict_regions["China, Macao SAR"] = "Macau"
+    dict_regions[
+        "Bonaire, Sint Eustatius and Saba"
+    ] = "Bonaire, Saint Eustatius and Saba"
+    dict_regions["Curaçao"] = "Curacao"
+    dict_regions["Saint Barthélemy"] = "St. Barths"
+    dict_regions["Saint Martin (French part)"] = "Saint-Martin"
+    dict_regions["Micronesia (Fed. States of)"] = "Micronesia, Fed. Sts."
+    dict_regions["Micronesia, Federated State=s of"] = "Micronesia, Fed. Sts."
+    dict_regions["Bonaire"] = "Bonaire, Saint Eustatius and Saba"
+    dict_regions["São Tomé and Principe"] = "Sao Tome and Principe"
+    dict_regions["Virgin Islands, British"] = "British Virgin Islands"
+    dict_regions["Wallis and Futuna"] = "Wallis and Futuna Islands"
+    dict_regions["Micronesia, Federated States of"] = "Micronesia, Fed. Sts."
+
+    dict_regions["VIR"] = "Virgin Islands, U.S."
+    dict_regions["GMB"] = "The Gambia"
+    dict_regions["NAM"] = "Namibia"
+    dict_regions["BHS"] = "The Bahamas"
+
+    for j in [
+        "Africa Eastern and Southern",
+        "Africa Western and Central",
+        "Arab World",
+        "Caribbean small states",
+        "Central Europe and the Baltics",
+        "Early-demographic dividend",
+        "East Asia & Pacific",
+        "East Asia & Pacific (excluding high income)",
+        "East Asia & Pacific (IDA & IBRD countries)",
+        "Euro area",
+        "Europe & Central Asia",
+        "Europe & Central Asia (excluding high income)",
+        "Europe & Central Asia (IDA & IBRD countries)",
+        "European Union",
+        "Fragile and conflict affected situations",
+        "Heavily indebted poor countries (HIPC)",
+        "High income",
+        "IBRD only",
+        "IDA & IBRD total",
+        "IDA blend",
+        "IDA only",
+        "IDA total",
+        "Late-demographic dividend",
+        "Latin America & Caribbean",
+        "Latin America & Caribbean (excluding high income)",
+        "Latin America & the Caribbean (IDA & IBRD countries)",
+        "Least developed countries: UN classification",
+        "Low & middle income",
+        "Low income",
+        "Lower middle income",
+        "Middle East & North Africa",
+        "Middle East & North Africa (excluding high income)",
+        "Middle East & North Africa (IDA & IBRD countries)",
+        "Middle income",
+        "North America",
+        "Not classified",
+        "OECD members",
+        "Other small states",
+        "Pacific island small states",
+        "Post-demographic dividend",
+        "Pre-demographic dividend",
+        "Small states",
+        "South Asia",
+        "South Asia (IDA & IBRD)",
+        "Sub-Saharan Africa",
+        "Sub-Saharan Africa (excluding high income)",
+        "Sub-Saharan Africa (IDA & IBRD countries)",
+        "Upper middle income",
+        "World",
+        "Arab League states",
+        "China and India",
+        "Czechoslovakia",
+        "East Asia & Pacific (all income levels)",
+        "East Asia & Pacific (IDA & IBRD)",
+        "East Asia and the Pacific (IFC classification)",
+        "EASTERN EUROPE",
+        "Europe & Central Asia (all income levels)",
+        "Europe & Central Asia (IDA & IBRD)",
+        "Europe and Central Asia (IFC classification)",
+        "European Community",
+        "High income: nonOECD",
+        "High income: OECD",
+        "Latin America & Caribbean (all income levels)",
+        "Latin America & Caribbean (IDA & IBRD)",
+        "Latin America and the Caribbean (IFC classification)",
+        "Low income, excluding China and India",
+        "Low-income Africa",
+        "Middle East & North Africa (all income levels)",
+        "Middle East & North Africa (IDA & IBRD)",
+        "Middle East (developing only)",
+        "Middle East and North Africa (IFC classification)",
+        "Other low-income",
+        "Serbia and Montenegro",
+        "Severely Indebted",
+        "South Asia (IFC classification)",
+        "Sub-Saharan Africa (all income levels)",
+        "SUB-SAHARAN AFRICA (excl. Nigeria)",
+        "Sub-Saharan Africa (IDA & IBRD)",
+        "Sub-Saharan Africa (IFC classification)",
+        "WORLD",
+        "UN development groups",
+        "More developed regions",
+        "Less developed regions",
+        "Least developed countries",
+        "Less developed regions, excluding least developed countries",
+        "Less developed regions, excluding China",
+        "Land-locked Developing Countries (LLDC)",
+        "Small Island Developing States (SIDS)",
+        "World Bank income groups",
+        "High-income countries",
+        "Middle-income countries",
+        "Upper-middle-income countries",
+        "Lower-middle-income countries",
+        "Low-income countries",
+        "No income group available",
+        "Geographic regions",
+        "Latin America and the Caribbean",
+        "Sustainable Development Goal (SDG) regions",
+        "SUB-SAHARAN AFRICA",
+        "NORTHERN AFRICA AND WESTERN ASIA",
+        "CENTRAL AND SOUTHERN ASIA",
+        "EASTERN AND SOUTH-EASTERN ASIA",
+        "LATIN AMERICA AND THE CARIBBEAN",
+        "AUSTRALIA/NEW ZEALAND",
+        "OCEANIA (EXCLUDING AUSTRALIA AND NEW ZEALAND)",
+        "EUROPE AND NORTHERN AMERICA",
+        "EUROPE",
+        "Holy See",
+        "NORTHERN AMERICA",
+        "East Asia & Pacific (ICP)",
+        "Europe & Central Asia (ICP)",
+        "Latin America & Caribbean (ICP)",
+        "Middle East & North Africa (ICP)",
+        "North America (ICP)",
+        "South Asia (ICP)",
+        "Sub-Saharan Africa (ICP)",
+    ]:
+        dict_regions[j] = "Z - Aggregated categories"
+    return None
+
+
+dict_regions_update()
+# all the regions that do not correspond to a country are in 'Z - Aggregated categories'
+# rename the appropriate level of dataframe using dict_regions
+def rename_region(df, level="LOCATION"):
+    """Renames the regions of a DataFrame into name_short format.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame whose regions must be renamed
+    level : string
+        Name of the level containing the region names
+
+    Returns
+    df : pd.DataFrame
+        DataFrame with regions in name_short format
+    -------
+    None
+    """
+    if level in df.index.names:
+        axis = 0
+    else:
+        axis = 1
+        df = df.T
+
+    index_names = df.index.names
+    df = df.reset_index()
+    df = df.set_index(level)
+    df = df.rename(index=dict_regions)  # rename index according to dict
+    ind = df.index.values
+    for i in range(0, len(ind), 1):
+        if type(ind[i]) == list:
+            # if len(ind[i])==0:
+            ind[i] = ind[i][0]
+    df = df.reindex(ind)
+    df = df.reset_index().set_index(index_names)
+    for i in df.index.get_level_values(level).unique():
+        if i not in name_short and i != i == "Z - Aggregated categories":
+            print(
+                i
+                + " is not in dict_regions\nAdd it using\n  >>> dict_regions['"
+                + i
+                + "'] = 'region' # name_short format\n"
+            )
+    if axis == 1:
+        df = df.T
+    return df
+
+
+# .........................
+
+QI = pd.read_csv(
+    "IHME_GBD_2016_HAQ_INDEX_1990_2016_SCALED_CAUSE_VALUES/IHME_GBD_2016_HAQ_INDEX_1990_2016_SCALED_CAUSE_VALUES_Y2018M05D23.csv",
+    header=0,
+    index_col=[1, 2, 4, 6],
+).xs("Healthcare Access and Quality Index", level=2)
+QI = rename_region(rename_region(QI, "location_name"), "ihme_loc_id")["val"].unstack()
+QI = QI.loc[[i[0] == i[1] for i in QI.index]]
+QI = QI.droplevel(0)
+
+population = pd.read_csv(
+    "worldbank population/API_SP.POP.TOTL_DS2_en_csv_v2_3852487.csv",
+    header=[2],
+    index_col=1,
+)
+population = rename_region(population, "Country Code")[
+    ["1990", "1995", "2000", "2005", "2010", "2016"]
+]
+population.loc["Taiwan"] = [20478520, 21356033, 21966527, 22705713, 23187551, 23618200]
+population = population.loc[QI.index]
+population.columns = [1990, 1995, 2000, 2005, 2010, 2016]
+
+pop_agg = population
+pop_agg["region"] = cc.convert(names=pop_agg.index, to="EXIO3")
+pop_agg = (
+    pop_agg.reset_index()
+    .set_index("region")
+    .drop("Country Code", axis=1)
+    .groupby(level="region")
+    .sum()
+)
+
+QI_agg = population * QI
+QI_agg["region"] = cc.convert(names=QI_agg.index, to="EXIO3")
+QI_agg = (
+    QI_agg.reset_index()
+    .set_index("region")
+    .drop("index", axis=1)
+    .groupby(level="region")
+    .sum()
+)
+QI_agg = (QI_agg / pop_agg)[[1995, 2000, 2005, 2010, 2016]]
+
+# ......
+
+
 def plt_rcParams():
     fsize = 10
     tsize = 4
@@ -620,12 +925,12 @@ def graph2():
     fig, axes = plt.subplots(1, 2, figsize=(9, 4))
 
     years = [1995, 2000, 2005, 2010, 2016]
-    x = QualityIndex[years]
-    y_pri = satellite_cap.loc[QualityIndex.index]["Energy Carrier Net Total"].unstack()
-    y_loss = satellite_cap.loc[QualityIndex.index]["Energy Carrier Net LOSS"].unstack()
+    x = QI_agg
+    y_pri = satellite_cap["Energy Carrier Net Total"].unstack()
+    y_loss = satellite_cap["Energy Carrier Net LOSS"].unstack()
     y1 = (y_pri - y_loss)[years]
     y2 = (
-        impacts_cap.loc[QualityIndex.index][
+        impacts_cap[
             [
                 "Domestic Extraction Used - Forestry and Timber",
                 "Domestic Extraction Used - Non-metalic Minerals",
@@ -635,9 +940,7 @@ def graph2():
         ]
         .sum(axis=1)
         .unstack()
-        + satellite_cap.loc[QualityIndex.index][
-            "Domestic Extraction Used - Fossil Fuel: Total"
-        ].unstack()
+        + satellite_cap["Domestic Extraction Used - Fossil Fuel: Total"].unstack()
     )[years]
 
     i = 0
@@ -660,7 +963,73 @@ graph2()
 # faire code couleur
 
 
-def graph3():
+def graph4():
+
+    fig, axes = plt.subplots(1, 2, figsize=(9, 4))
+    years = [i for i in range(2002, 2017, 1)]
+
+    y = (
+        (
+            (
+                satellite["Energy Carrier Net Total"]
+                - satellite["Energy Carrier Net LOSS"]
+            )
+            .unstack()
+            .drop(1995, axis=1)
+            .stack()
+        ).loc[constant_ppp.unstack().swaplevel().index]
+        / constant_ppp.unstack().swaplevel()
+        * 1000
+    )
+    x = (
+        constant_ppp.unstack().swaplevel()
+        / pop.stack().loc[constant_ppp.unstack().swaplevel().index]
+    )
+    k = 0
+
+    for reg in y.unstack().index:
+        for year in years:
+            axes[0].scatter(
+                np.log(x.loc[reg].loc[year]),
+                y.loc[reg].loc[year],
+                s=pop.loc[reg].loc[year] / 6000,
+                color=str(1 - (year - 2002) / 14),
+                edgecolors="black",
+            )
+            axes[1].scatter(
+                year,
+                y.loc[reg].loc[year],
+                s=pop.loc[reg].loc[year] / 6000,
+                color=str(k / 49),
+                edgecolors="black",
+            )
+        k += 1
+
+    y_world = (
+        (
+            (
+                satellite["Energy Carrier Net Total"]
+                - satellite["Energy Carrier Net LOSS"]
+            )
+            .unstack()
+            .drop(1995, axis=1)
+        )[years].sum()
+        / constant_ppp.sum()
+        * 1000
+    )
+    axes[1].plot(y_world.index, y_world)
+
+    axes[0].set_ylim(top=5.2)
+    axes[1].set_ylim(top=5.2)
+    axes[1].set_xlim(right=2018)
+    axes[1].set_xticks([2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016])
+
+
+graph4()
+# rajouter couleurs
+
+
+def graph9():
 
     fig, axes = plt.subplots(1, 2, figsize=(9, 4))
 
@@ -716,7 +1085,7 @@ def graph3():
             )
 
 
-graph3()
+graph9()
 
 
 def graph5():  # comparaisons déflation
